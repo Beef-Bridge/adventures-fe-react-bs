7c06139c4d0a9c13a9a7439532cd31ded4c51ac7
Nikoquattro <nikoquattro@gmail.com>
2023-01-29 21:34:42 +0100

    creation du répertoire src/ et du fichier App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+const App: FunctionComponent = () => {
+    const name: String = 'Adventures FE React BS';
+
+    return (
+        <h1>Hello, {name} !</h1>
+    )
+}
+
+export default App;
