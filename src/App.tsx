38c10fa15842a9590429cb640f7ca2b3bc6cb00b
Nikoquattro <nikoquattro@gmail.com>
2023-01-29 22:01:29 +0100

    creation hook d'état et hook d'effet pour utiliser mock-hikes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import Hike from './models/hike';
+import HIKES from './models/mock-hike';
 
 const App: FunctionComponent = () => {
-    const name: String = 'Adventures FE React BS';
+    const [hikes, setHikes] = useState<Hike[]>([]);
+
+    useEffect(() => {
+        setHikes(HIKES);
+    }, []);
 
     return (
-        <h1>Hello, {name} !</h1>
+        <div>
+            <h1>Adventures FE React BS</h1>
+            <p>Il y a {hikes.length} randonnées dans l'application.</p>
+        </div>
     )
 }
 
