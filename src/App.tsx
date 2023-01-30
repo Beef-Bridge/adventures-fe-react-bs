62c8f23ef5e0aeb9c0b9d1e3d8ebc0bb04f1cc8a
Nikoquattro <nikoquattro@gmail.com>
2023-01-30 21:39:42 +0100

    affichage de la liste des randos avec utillisation d'une boucle et application template Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,24 @@ const App: FunctionComponent = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Adventures FE React BS</h1>
-            <p>Il y a {hikes.length} randonnées dans l'application.</p>
-        </div>
+        <div className='container px-4 py-5'>
+            <h1 className='pb-2 border-bottom'>Adventures FE React BS</h1>
+            <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5'>
+                {hikes.map(({ id, title, picture, created }) => (
+                    <div className='col d-flex align-items-start'>
+                        <div className='col-md-12' key={id}>
+                            <div className='card'>
+                                <img className='card-img-top' src={picture} alt={title} />
+                                <div className='card-body'>
+                                    <h5 class='card-title'>{title}</h5>
+                                    <p className='card-text'>{created.toString()}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div >
     )
 }
 
